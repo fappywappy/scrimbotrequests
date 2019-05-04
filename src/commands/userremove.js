d3c88a784a14f525d430cd37fd1bf0625469b55e
John <sedansvan@gmail.com>
2019-05-03 17:38:45 -0700

    removed error

--- src/commands/userremove.js
+++ src/commands/userremove.js
@@ -45,13 +45,7 @@ module.exports = async function (bot, args, msg) {
   if (!found) {
     return errorMsg(msg, `${rawUser} not found in a [team](${bot.team_panel_1.url}).`);
   }
-  `**__Statist`**__Statistics__**\nGame 1: #${teams[i].game1position}\nGame 2: #${teams[i].game2position}\nGame 3: #${teams[i].game3position}\nKills: ${teams[i].game1kills + teams[i].game2kills + teams[i].game3kills}\nPoints: ${teams[i].points}\n\n**__Players__**\n` + 
-  (players.reduce((str, p, idx) => {
-    return str + `- <@${p}>${idx < players.length - 1 ? '\n' : (players.length === 4) ? '' : '\n'}`
-  }, '')) + emptyPlayersStr + '\n--------------------------------'ics__**\nGame 1: #${teams[i].game1position}\nGame 2: #${teams[i].game2position}\nGame 3: #${teams[i].game3position}\nKills: ${teams[i].game1kills + teams[i].game2kills + teams[i].game3kills}\nPoints: ${teams[i].points}\n\n**__Players__**\n` + 
-  (players.reduce((str, p, idx) => {
-    return str + `- <@${p}>${idx < players.length - 1 ? '\n' : (players.length === 4) ? '' : '\n'}`
-  }, '')) + emptyPlayersStr + '\n--------------------------------'
+
   saveResources(bot);
   updateTeams(bot);
 
